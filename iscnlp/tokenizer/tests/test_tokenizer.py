0af72921513047b1f8376feb262c055a70a96c80
Irshad Ahmad Bhat <bhatirshad127@gmail.com>
2016-08-16 21:31:24 +0530

    test command line argument file processing

--- iscnlp/tokenizer/tests/test_tokenizer.py
+++ iscnlp/tokenizer/tests/test_tokenizer.py
@@ -5,7 +5,7 @@ import io
 import os
 
 from testtools import TestCase
-from iscnlp.tokenizer import Tokenizer, parse_args
+from iscnlp.tokenizer import Tokenizer, parse_args, process_args
 
 
 class TestTokenizer(TestCase):
@@ -25,6 +25,7 @@ class TestTokenizer(TestCase):
                     self.assertIsInstance(tokenized_text, list)
 
     def test_parser(self):
+        # test parser arguments
         parser = parse_args(['--input', 'path/to/input_file',
                              '--output', 'path/to/output_file',
                              '--language', 'kas',
@@ -33,3 +34,8 @@ class TestTokenizer(TestCase):
         self.assertEqual(parser.outfile, 'path/to/output_file')
         self.assertEqual(parser.lang, 'kas')
         self.assertTrue(parser.split_sen)
+        # test parser args processing
+        process_args(parse_args(['-i', '%s/eng.txt' % self.test_dir,
+                                 '-o', '/tmp/test.out',
+                                 '-l', 'eng',
+                                 '-s']))
