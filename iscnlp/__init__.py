71f497e479efddc96b850de4486451ce3815d977
riyazbhat <riyaz.ah.bhat@gmail.com>
2016-08-19 01:27:52 +0530

    added parser for Hindi

--- iscnlp/__init__.py
+++ iscnlp/__init__.py
@@ -1,5 +1,6 @@
 from iscnlp.tagger import Tagger
+from iscnlp.parser import Parser
 from iscnlp.tokenizer import Tokenizer
 from iscnlp.mlp import MultiLayerPerceptron
 
-__all__ = ['MultiLayerPerceptron', 'Tagger', 'Tokenizer']
+__all__ = ['MultiLayerPerceptron', 'Tagger', 'Tokenizer', 'Parser']
