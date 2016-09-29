9e9f7c09e6d4146aa44a8dd9d1b398d8a9b1b8f3
Irshad Ahmad Bhat <bhatirshad127@gmail.com>
2016-09-29 19:22:19 +0530

    handle text differently (whether called from console or from python directly)

--- iscnlp/tokenizer/tokenizer.py
+++ iscnlp/tokenizer/tokenizer.py
@@ -5,11 +5,21 @@ from .roman_tokenizer import RomanTokenizer
 
 
 class Tokenizer():
-    def __init__(self, lang='hin', split_sen=False):
-        if lang == 'eng':
-            self.tok = RomanTokenizer(split_sen=split_sen)
+    def __init__(self, lang='eng', split_sen=False, tweets=False, from_file=False):
+        self.from_file = from_file
+        self.split_sen = split_sen
+        if lang in ['eng', 'spa']:
+            self.tok = RomanTokenizer(lang=lang, split_sen=split_sen, tweets=tweets)
         else:
-            self.tok = IndicTokenizer(lang=lang, split_sen=split_sen)
+            self.tok = IndicTokenizer(lang=lang, split_sen=split_sen, tweets=tweets)
 
     def tokenize(self, sentence):
-        return self.tok.tokenize(sentence)
+        if self.from_file or not self.split_sen:
+            return self.tok.tokenize(sentence)
+        else:
+            out_sents = []
+            sentences = sentence.split('\n')
+            for sent in sentences:
+                tok_sent = self.tok.tokenize(sent)
+                out_sents.extend(tok_sent)
+            return out_sents
